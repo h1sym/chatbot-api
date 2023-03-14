824f2ad9d91ace45b3e070f6c79a9d97b7fc69f7
zhengkaixiang <2285674680@qq.com>
2023-03-14 17:35:21 +0800

    郑凯祥

---
+++ chatbot-api-interfaces/src/main/java/com/zhengkaixiang/chatbot/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.zhengkaixiang.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhengkaixiang
+ * @description
+ * @created 2023/3/14-16:06
+ **/
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/com/zhengkaixiang/chatbot/api/ApiTest.java
@@ -0,0 +1,9 @@
+package com.zhengkaixiang.chatbot.api;
+
+/**
+ * @author zhengkaixiang
+ * @description
+ * @created 2023/3/14-16:59
+ **/
+public class ApiTest {
+}
